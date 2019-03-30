54fde6c39956e4f79d1e721d01267fcd2c52a227
aaezha <aaezha@gmail.com>
2019-03-30 07:06:31 +0700

    Update meta tag

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,9 +6,28 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Sistem Informasi Manajemen Pesantren La Tansa">
+  <meta name="description" content="Sistem Inventori Buku Perpustakaan Pribadi Sederhana">
   <meta name="author" content="Reza Nurfachmi">
-  <link rel="shortcut icon" href="<?=base_url('assets/img/');?>logo/favicon.ico" type="image/x-icon"/>
+  <link rel="shortcut icon" href="<?=base_url('assets/img/');?>buku.png" type="image/x-icon"/>
+
+  <!--  Essential META Tags -->
+
+  <meta property="og:title" content="Bukuku - Reza Nurfachmi">
+  <meta property="og:description" content="Sistem Inventori Buku Perpustakaan Pribadi Sederhana">
+  <meta property="og:image" content="<?=base_url('assets/img/');?>buku.png">
+  <meta property="og:url" content="<?=base_url();?>">
+  <meta name="twitter:card" content="Sistem Inventori Buku Perpustakaan Pribadi Sederhana">
+
+
+  <!--  Non-Essential, But Recommended -->
+
+  <meta property="og:site_name" content="Nurfachmi, Inc.">
+  <meta name="twitter:image:alt" content="Sistem Inventori Buku Perpustakaan Pribadi Sederhana">
+
+
+  <!--  Non-Essential, But Required for Analytics -->
+
+  <meta name="twitter:site" content="@website-aaezha">
 
   <title><?=$title;?> - Reza Nurfachmi</title>
 
